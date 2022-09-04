95bb78aa6749642be903bc537faf1e205a555c0e
Sophie <107502956+S0PHI3K@users.noreply.github.com>
2022-09-04 19:56:53 +0100

    Update BigO-2NumSum.py

--- BigO-2NumSum.py
+++ BigO-2NumSum.py
@@ -3,13 +3,16 @@
 #You can return the answer in any order.
 #Take into consideration Big O Time & Space Complexity
 
-def two_number_sum(target_sum):
-    my_numbers = [3, 5, -4, 8, 11, 1, -1, 6]
-    for num1 in my_numbers:
-        for num2 in my_numbers:
-            if (target_sum == num1 + num2) and (num1 != num2):
-                return [num1, num2]
+def twoSum(numbers, target):
+    hashTable = {}
+    for num in range(len(numbers)):
+        match = target - numbers[num]
+        if match in hashTable:
+            print(numbers[num], match)
+        hashTable[numbers[num]] = numbers[num]
     else:
-        return[]
+        return []
 
-print(two_number_sum(10))
+numbers = [3,7,8,2,10,-1]
+target = 10
+twoSum(numbers, target)
